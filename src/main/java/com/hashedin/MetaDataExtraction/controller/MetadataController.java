91bff60a9af5c8090194afd9234631b5ac1097db
rudrkrishna <Kittu2909@>
2023-02-08 11:44:46 +0530

    Code cleanup

--- src/main/java/com/hashedin/MetaDataExtraction/controller/MetadataController.java
+++ src/main/java/com/hashedin/MetaDataExtraction/controller/MetadataController.java
@@ -1,11 +1,9 @@
 package com.hashedin.MetaDataExtraction.controller;
 
-import com.hashedin.MetaDataExtraction.service.MetaDataServiceImpl;
 import com.hashedin.MetaDataExtraction.service.WorkSpaceMetaDataServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -13,16 +11,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class MetadataController {
 
-    private final MetaDataServiceImpl metaDataService;
     private final WorkSpaceMetaDataServiceImpl workSpaceMetaDataService;
     @Autowired
-    public MetadataController(MetaDataServiceImpl metaDataService, WorkSpaceMetaDataServiceImpl workSpaceMetaDataService) {
-        this.metaDataService = metaDataService;
+    public MetadataController(WorkSpaceMetaDataServiceImpl workSpaceMetaDataService) {
         this.workSpaceMetaDataService = workSpaceMetaDataService;
     }
 
-    @GetMapping("/workSpaceMetaData/{workSpaceId}")
-    public ResponseEntity<String> migrateWorkspaceMetaData(@PathVariable String workSpaceId){
+    @GetMapping("/workSpaceMetaData")
+    public ResponseEntity<String> migrateWorkspaceMetaData(@RequestParam String workSpaceId){
         String workspaceStatus=workSpaceMetaDataService.migrateMetaData(workSpaceId);
         if(workspaceStatus.contains("Incorrect")){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(workspaceStatus);
